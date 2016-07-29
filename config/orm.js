ef6da871c43c999a33c3f15a787504153a0cde01
Lee Warnock <clwarnock@gmail.com>
2016-07-28 21:11:54 -0500

    adjusts methods in orm.js

--- config/orm.js
+++ config/orm.js
@@ -2,21 +2,21 @@ var connection = require('./connection.js');
 
 
 var orm = {
-  addBurger: function(tableInput, nameInput, cb) {
+  insertOne: function(tableInput, nameInput, cb) {
     var s = "INSERT INTO " + tableInput + " (burger_name) VALUES (?)";
     connection.query(s, [nameInput], function(err, result) {
       if (err) throw err;
       cb(result);
     });
   },
-  eatBurger: function(tableInput, idInput, cb) {
-    var s = "UPDATE " + tableInput + " SET eatBurger = 1  WHERE burger_name = ?";
+  updateOne: function(tableInput, idInput, cb) {
+    var s = "UPDATE " + tableInput + " SET updateOne = 1  WHERE burger_name = ?";
     connection.query(s, [idInput], function(err, result) {
       if (err) throw err;
       cb(result);
     });
   },
-  allBurgers: function(cb) {
+  selectAll: function(cb) {
     var s = 'SELECT * FROM burgers';
     connection.query(s, function(err, result) {
       if (err) throw err;
