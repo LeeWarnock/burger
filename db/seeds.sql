a4890fdf8ecfba13e3bb2ddf748ffd23c41a7fcf
Lee Warnock <clwarnock@gmail.com>
2016-07-28 20:26:37 -0500

    adds seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers(burger_name) VALUES ('Runny Egg Burger');
+INSERT INTO burgers(burger_name) VALUES ('Questionably-Sourced Burger');
+INSERT INTO burgers(burger_name) VALUES ('Invisiburger');
+INSERT INTO burgers(burger_name) VALUES ('Inside-Out Burger');
+INSERT INTO burgers(burger_name) VALUES ('Chicken Burger');
+INSERT INTO burgers(burger_name) VALUES ('Bourgeois Burger');
+INSERT INTO burgers(burger_name) VALUES ('Chicken Fried Burger');
